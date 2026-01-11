991a11f39319b9fdb0cbb6ce23cb6ce0fe43003b
Raspberry-Monster <lele2194512339@outlook.com>
2026-01-11 14:19:26 +0800

    Upload project files

---
+++ Pivot/PivotTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Hosting;
+
+namespace Pivot
+{
+    public partial class PivotTest1 : Windows.UI.Xaml.Controls.Pivot
+    {
+        public PivotTest1()
+        {
+            var test = ElementCompositionPreview.GetElementVisual(this);
+        }
+    }
+}
